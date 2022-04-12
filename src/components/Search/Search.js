d7224e060b6edbdee6c185ef95bd5e16cc9edbe1
bekir burak aslan <bekirburak.aslan16@gmail.com>
2022-04-12 16:47:18 +0300

    Add files via upload

---
+++ src/components/Search/Search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Search.css";
+import searchIcon from "../../../src/search-icon.png";
+
+const Search = (props) => {
+  return (
+    <div className="searchBar">
+      <div>
+        <input
+          type="text"
+          placeholder="Search City"
+          maxLength="50"
+          value={props.city}
+        />
+        <a
+        >
+          <img src={searchIcon}></img>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
